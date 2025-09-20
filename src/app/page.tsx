40c65cdff852dd07dd6b9eb3ae42cbc0cf8fd553
sandreannyfarias99-lang <sandreannyfarias99@gmail.com>
2025-09-20 13:48:37 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import FeaturedCampaigns from '@/components/FeaturedCampaigns'
 import LocalFeatures from '@/components/LocalFeatures'
-import HowItWorks from '@/components/HowItWorks'
+import HowITWorks from '@/components/HowItWorks'
 import Testimonials from '@/components/Testimonials'
 import Footer from '@/components/Footer'
 
